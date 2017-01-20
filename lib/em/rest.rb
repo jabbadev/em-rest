060da530f2c94275493c595de5f68d0ee2eb06cf
Francesco Dalprà <jabba.dev@gmail.com>
2017-01-20 17:32:35 +0100

    Refactor

--- lib/em/rest.rb
+++ lib/em/rest.rb
@@ -48,7 +48,30 @@ module EventMachine
           if ( httpVerb == 'POST' or httpVerb == 'PUT' ) and i == lastIndex
             params = args[:bodyReq]
           end
-         
+
+          if !@customHandler.nil? and @customHandler.respond_to?:call
+            methodHandled = true
+            resObj = @customHandler.call(@resources,args)
+          elsif !@customHandler.nil? and @customHandler.respond_to?method.to_sym
+            methodHandled = true
+            resObj = @customHandler.send(method.to_sym,@resources,args)
+          elsif !@customHandler.nil? and @customHandler.respond_to?:key
+            if @customHandler.key?method
+              custMeth = @customHandler[method]
+              if custMeth.respond_to?:call
+                methodHandled = true
+                resObj = custMeth.call(@resources,args)
+              end
+            elsif @customHandler.key?method.to_sym
+              custMeth = @customHandler[method.to_sym]
+              if custMeth.respond_to?:call
+                methodHandled = true
+                resObj = custMeth.call(@resources,args)
+              end
+            end 
+          end 
+          
+                  
           if resObj.respond_to?:call
             if params.is_a?Array
               resObj = resObj.call(@resources,args)
@@ -86,27 +109,7 @@ module EventMachine
                             
               methodHandled = false
                         
-              if !@customHandler.nil? and @customHandler.respond_to?:call
-                methodHandled = true
-                resObj = @customHandler.call(@resources,args)
-              elsif !@customHandler.nil? and @customHandler.respond_to?method.to_sym
-                methodHandled = true
-                resObj = @customHandler.send(method.to_sym,@resources,args)
-              elsif !@customHandler.nil? and @customHandler.respond_to?:key
-                if @customHandler.key?method
-                  custMeth = @customHandler[method]
-                  if custMeth.respond_to?:call
-                    methodHandled = true
-                    resObj = custMeth.call(@resources,args)
-                  end
-                elsif @customHandler.key?method.to_sym
-                  custMeth = @customHandler[method.to_sym]
-                  if custMeth.respond_to?:call
-                    methodHandled = true
-                    resObj = custMeth.call(@resources,args)
-                  end
-                end 
-              end 
+
               
               unless methodHandled
                 
